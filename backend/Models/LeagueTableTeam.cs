09fd5775b0584208acd4ddfbe91dca511da723b5
zivd <ziad99kr@gmail.com>
2026-01-27 18:44:06 +0100

    League Table class

---
+++ backend/Models/LeagueTableTeam.cs
@@ -0,0 +1,15 @@
+
+//LeagueTableTeam attributes
+
+public class LeagueTableTeam
+{
+    public int Position { get; set; }
+    public string Team { get; set; } = "";
+    public int Played { get; set; }
+    public int Won { get; set; }
+    public int Drawn { get; set; }
+    public int Lost { get; set; }
+    public int GoalDifference { get; set; }
+    public int Points { get; set; }
+    public string Form { get; set; } = "";
+}
