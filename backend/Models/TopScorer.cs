2a9089051a2b38fc29d81dd382c72757133c7633
zivd <ziad99kr@gmail.com>
2026-01-30 12:44:33 +0100

    top scorer class

---
+++ backend/Models/TopScorer.cs
@@ -0,0 +1,11 @@
+
+
+public class TopScorer
+{
+    public int Rank { get; set; }
+    public string Player { get; set; } = "";
+    public string Team { get; set; } = "";
+    public int Goals { get; set; }
+    public int Assists { get; set; }
+    public int Matches { get; set; }
+}
